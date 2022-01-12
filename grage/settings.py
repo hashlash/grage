2873c58c24d3fd5e72e7b30372f62ff83aeb4b85
hashlash <muh.ashlah@gmail.com>
2022-01-12 17:41:15 +0700

    Add static configuration

--- grage/settings.py
+++ grage/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
